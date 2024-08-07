4b26922aa02ca0a5210492e987d3bc9719cebfc9
sagnik <sghosh@uni-bonn.de>
2024-08-07 22:15:00 +0200

    [script] - runfile, jobfile adjusted for BAF rerouting

--- create_file.sh
+++ create_file.sh
@@ -2,14 +2,15 @@
 
 #creates job iteratively 
 
+L=8
 
-for i1 in {1..1000}
+for i1 in {1..1}
 do
 
 
 
 
-cat <<EOF >run_file_hcee10_${i1}.sh
+cat <<EOF >run_file_mbldtcL${L}_${i1}.sh
 #!/bin/bash
      
   
@@ -22,15 +23,15 @@ source /etc/profile
 
 #Copies, Extracts and removes the Julia tarball 
             
-cp \$BUDDY/julia-1.8.3.tar.gz ./
+cp ~/local/julia/julia-07-08-24.tar.gz ./
 
-tar -xf julia-1.8.3.tar.gz
-rm -f julia-1.8.3.tar.gz
+tar -xf julia-07-08-24.tar.gz
+rm -f julia-07-08-24.tar.gz
 
 
 
 #Loads Julia
-module load julia/1.8.3
+module load julia/1.9.4
 
 #Creates folder for the Job
 
@@ -40,8 +41,8 @@ cd \${ClusterId}_\$Process
 
 #Job submission 
 
-cp \$BUDDY/halfchainL10_${i1}.jl ./
-cp -rf \$BUDDY/header ./
+cp ~/local/mbldtc_L${L}_${i1}.jl ./
+cp -rf ~/local/BAF/header ./
 
 # set number of threads
 export OPENBLAS_NUM_THREADS=1
@@ -49,16 +50,12 @@ export OMP_NUM_THREADS=1
 export JULIA_NUM_THREADS=1
 
 # Do the real thing here
-julia halfchainL10_${i1}.jl
-rm halfchainL10_${i1}.jl
-
-# Make sure the folder exists
-#mkdir -p \$BUDDY/log
+julia mbldtcL${L}_${i1}.jl
+rm mbldtcL{L}_${i1}.jl
 
 
 # copy results
-#cp -r /jwd/\${ClusterId}_\$Process \$BUDDY/Data
-cp  /jwd/\${ClusterId}_\$Process/*.hdf5 \$BUDDY/Data
+cp  /jwd/\${ClusterId}_\$Process/*.hdf5 ~/local/data/.
 EOF
 
 
@@ -67,11 +64,11 @@ EOF
 
 
 
-cat <<EOF >job_file_hcee8_${i1}.jdl
+cat <<EOF >job_file_mbldtcL${L}_${i1}.jdl
 
 #Job Script to be submitted using HTCondor
         
-Executable              = run_file_hcee10_${i1}.sh
+Executable              = run_file_mbldtcL${L}_${i1}.sh
         
 Environment             = ClusterId=\$(ClusterId);Process=\$(Process);SubHost=$ENV(SUBHOST);
         
@@ -88,14 +85,14 @@ Error                   = log/err.\$(ClusterId).\$(Process)
 Output                  = log/out.\$(ClusterId).\$(Process)
 Log                     = log/log.\$(ClusterId).\$(Process)
 
-Request_memory          = 40  GB
+Request_memory          = 8  GB
 Request_cpus            = 1
         
 Request_disk            = 2 GB
 
 +CephFS_IO  = "low"
         
-+MaxRuntimeHours	= 48
++MaxRuntimeHours	= 1
         
 +ContainerOS            = "Debian11"
 
