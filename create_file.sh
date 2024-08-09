518a464bf172720b025bea1c1acadc05f63ebc98
sagnik <sghosh@uni-bonn.de>
2024-08-09 15:16:31 +0200

    [file] eig, halfchain script

--- create_file.sh
+++ create_file.sh
@@ -37,8 +37,7 @@ include("header/functions.jl")
 
 L=${L};
 theta=0.0;
-epsilon=0.0; 
-
+Itrnumb=2;
 
 
 file=h5open("mbldtc_L8_theta_\$(theta)_${i1}.hdf5","cw")
@@ -89,27 +88,86 @@ attrs=attributes(file)
 
 	# Meta Data
 
-#########################################################################
-# Brickwall
-#########################################################################
+	attrs["[Parameters] L"] = L
+	attrs["[Parameters] theta"] = theta
+	attrs["[Parameters] epsilon"] = string(epsilonlist)
+
+######################################
+# Code:
+######################################	
 
 
-U=brickwall(L,theta,epsilon);
+epsilonlist=[0.0,0.025,0.05,0.075,0.1,0.125,0.15,0.175,0.2,0.225,0.25,0.275,0.3,0.325,0.35,0.375,0.4,0.425,0.45,0.475,0.5,0.525,0.55,0.575,0.6,0.625,0.65,0.675,0.7,0.725,0.75,0.775,0.8,0.825,0.85,0.875,0.9,0.925,0.95,0.975,1.0]
+
+levelspacing=fill(0.0,length(epsilonlist))
+entanglement_ee=fill(0.0,length(epsilonlist))
+
+
+
+
+
+for i in 1:length(epsilonlist)
+
+	epsilon=epsilonlist[i]
+
+
+	for itr in 1:\$(Itrnumb)
+	
+	#########################################################################
+	# Brickwall
+	#########################################################################
+
+
+	U=brickwall(L,theta,epsilon);
+
+	#h=rand(L)*pi/2;
+	#Ind=collect(1:L)
+	#ZRow=copy(kronlist(RZ.(h),Ind));
+
+
+
+
+	A=brickwall(L,theta,epsilon)
+	#A=A*ZRow
+
+
+
 
 
 #########################################################################
 # Eigenstatistics
 #########################################################################
+	eigA,eigvecA=eigen(A)
 
-eigA,eigvecA=eigen(U);
+	#saving the eigenvalues
+	file["L\$(L)/theta\$(theta)/epsilon"*first("\$(epsilon)",5)*"/Itr\$(itr)"]=eigA;
+	
+	#computing the level spacing
+	levelspacing[i]=levelspacing[i]+LevelSpacingRatio(eigA)
+    
+
 
 #########################################################################
 # Eigenstate Entanglement Entropy
 #########################################################################
 
+for j in 1:length(eigvecA)
+	
+	#computing the half chain entanglement entropy
+	entanglement_ee[i]=entanglement_ee[i]+EntanglementEntropy(eigvecA[j],L/2)
+	
+end
+
+
 #########################################################################
 # Lazarides-Luitz Staistics 
 #########################################################################
+
+	end
+
+end
+
+levelspacing=levelspacing/\$(Itrnumb)
 
 
 file["L\$(L)/theta\$(theta)/epsilon"*first("\$(epsilon)",4)*"/Itr"]=eigA;
