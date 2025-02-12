84453819bdc8c9af5a2addddf759b6185c69f1ea
sagnik <sghosh@uni-bonn.de>
2025-02-12 17:18:49 +0100

    [opo] script file

---
+++ OPO/create_env.sh
@@ -0,0 +1,118 @@
+#!/bin/bash
+
+#creates job iteratively 
+
+cd ../../..
+fp=0.1
+
+#cd $BUDDY
+
+Itrnumb=1
+
+for i1 in {1..Itrnumb}
+do
+
+
+
+
+
+
+
+cat <<EOF >run_file_OPO_fp${fp}_${i1}.sh
+#!/bin/bash
+     
+  
+     
+# Goes to the job work direcrtory     
+            
+cd /jwd			
+source /etc/profile
+
+
+
+#Copies, Extracts and removes the Julia tarball 
+            
+cp \$BUDDY/julia/julia-1.9.4-08-08-24.tar.gz ./
+
+tar -xf julia-1.9.4-08-08-24.tar.gz
+rm -f julia-1.9.4-08-08-24.tar.gz
+
+
+
+#Loads Julia
+module load julia/1.9.4
+
+#Creates folder for the Job
+
+#mkdir \${ClusterId}_\$Process
+#cd \${ClusterId}_\$Process
+
+
+#Job submission 
+
+cp \$BUDDY/opo_fp_${fp}_${i1}.jl ./
+cp -rf \$BUDDY/BAF/OPO/header ./
+
+# set number of threads
+export OPENBLAS_NUM_THREADS=1
+export OMP_NUM_THREADS=1
+export JULIA_NUM_THREADS=1
+
+# Do the real thing here
+julia opo_fp_${fp}_${i1}.jl
+rm opo_fp_${fp}_${i1}.jl
+
+
+# copy results
+cp  /jwd/\${ClusterId}_\$Process/*.hdf5 /cephfs/user/sghosh/data/.
+rm -rf /jwd/\${ClusterId}_\$Process
+EOF
+
+
+
+
+
+
+
+cat <<EOF >job_file_OPO_fp${fp}_${i1}.jdl
+
+#Job Script to be submitted using HTCondor
+        
+Executable              = run_file_OPO_fp${fp}_${i1}.sh
+        
+Environment             = ClusterId=\$(ClusterId);Process=\$(Process);SubHost=$ENV(SUBHOST);
+        
+Arguments		= 8
+        
+Universe                = vanilla
+
+Transfer_executable     = 
+Transfer_input_files    = 
+Transfer_output_files   =
+
+
+Error                   = log/err.\$(ClusterId).\$(Process)
+Output                  = log/out.\$(ClusterId).\$(Process)
+Log                     = log/log.\$(ClusterId).\$(Process)
+
+Request_memory          = 12  GB
+Request_cpus            = 1
+        
+Request_disk            = 2 GB
+
++CephFS_IO  = "low"
+        
++MaxRuntimeHours	=   48
+        
++ContainerOS        = "Debian12"
+
+# Submit job
+Queue 1        
+            
+EOF
+
+
+done
+
+
+
