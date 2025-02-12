a938b5c93e2779e7ea553a87217216c18d1e9958
sagnik <sghosh@uni-bonn.de>
2025-02-12 17:13:42 +0100

    [script] file

--- OPO/create_file.sh
+++ OPO/create_file.sh
@@ -46,14 +46,14 @@ T = 10.0         			# Total time
 noise_strength = sqrt(kappa/dx)*sqrt(dt)  
 							# Strength of the noise
 
-f_0=\$(fp) 					# Pumping strength
+f_0=${fp} 					# Pumping strength
 k_p=1.4						# Pumping wave number
 omega_p=-0.42;				# Pumping frequency
 
 
 
 
-file=h5open("OPO_fp_\$(fp)_${i1}.hdf5","cw")
+file=h5open("OPO_fp_${fp}_${i1}.hdf5","cw")
 attrs=attributes(file)
 
 
@@ -81,7 +81,7 @@ attrs=attributes(file)
 
 	# Container OS
 
-	attrs["[ENV] Container OS"] = "Debian11"
+	attrs["[ENV] Container OS"] = "Debian12"
 
 	# Extracting Number of Threads
 
@@ -90,8 +90,13 @@ attrs=attributes(file)
 
 	# Code
 
-	script_content = read("mbldtc_L${L}_${i1}.jl", String)
+	script_content = read("opo_fp_${fp}_${i1}.jl", String)
 	attrs["[ENV] Code"] = script_content
+
+	# Header
+
+	header_content = read("header/Headers.jl", String)
+	attrs["[ENV] Header"] = header_content
 
 	# Modules
 
@@ -104,7 +109,7 @@ attrs=attributes(file)
 
 	# Meta Data
 
-	attrs["METADATA"] = "This Data file contains a full parameter scan of epsilon in epsilonlint of the MBL-DTC Unitary for L=\$(L) and theta=\$(theta). The data contains the eigenvalues for each realisation, disorder averaged level spacing ratio, disorder averaged eigestate entanglement entropy and disorder averaged histograms diagonal, pi-diagonal and various offdiagonal elements of the Lazadires Matrix."
+	attrs["METADATA"] = "This Data file contains a RK4 simulation of the OPO model with a fixed pumping strength and a fixed noise strength for one iteration. We save the full dynamics for certain sites and the full wavefunction for certain time steps."
 
 	# Author
 
@@ -117,10 +122,8 @@ attrs=attributes(file)
 	# Parameters
 
 	attrs["[Parameters] h"] = "0"
-	attrs["[Parameters] L"] = L
-	attrs["[Parameters] theta"] = theta
-	attrs["[Parameters] epsilon"] = string(epsilonlist)
-
+	attrs["[Parameters] fp"] = f_0
+	
 	attrs["[Parameters] Itrnumb"] = Itrnumb
 
 
@@ -131,121 +134,57 @@ attrs=attributes(file)
 
 
 
-for i in 1:length(epsilonlist)
+	# Spatial grid
+	x = range(0, L-dx, step=dx)
 
-	epsilon=epsilonlist[i]
+	# Generate complex Gaussian noise
 
-
-	global histogram_diag=fill(0,2000)
-	global histogram_pi_diag=fill(0,2000)
-
-	global histogram_offdiag=fill(0,16,2000)
-	global histogram_pi_offdiag=fill(0,16,2000)
-
-
-	for itr in 1:Itrnumb
-
-
-		#########################################################################
-		# Brickwall
-		#########################################################################
+	global u_final = fill(0.0*im, N)
+	global tseries = fill(0.0*im, 10001)
 
 
 
-		#h=rand(L)*pi/2;
-		#Ind=collect(1:L)
-		#ZRow=copy(kronlist(RZ.(h),Ind));
+	time_series_1 = Complex{Float64}[]
+	time_series_2 = Complex{Float64}[]
+	time_series_3 = Complex{Float64}[]
+	time_series_4 = Complex{Float64}[]
+	time_series_5 = Complex{Float64}[]
+	time_series_6 = Complex{Float64}[]
 
+	# Initial condition
+	u = zeros(Complex{Float64}, N)
 
-
-
-		A=brickwall(L,theta,epsilon)
-		#A=A*ZRow
-
-
-
-
-
-		#########################################################################
-		# Eigenstatistics
-		#########################################################################
-		
-		eigA,eigvecA=eigen(A)
-
-		#saving the eigenvalues
-		file["L\$(L)/theta\$(theta)/epsilon"*first("\$(epsilon)",5)*"/Itr\$(itr)"]=eigA;
+	# Time-stepping
 	
-		#computing the level spacing
-		levelspacing[i]=levelspacing[i]+LevelSpacingRatio(eigA)
-    
+	t = 0.0
+	while t < T
+    u = rk4_step_full(u, dt, t, g, f_0, kappa, dx).+noise_strength*randn(Complex{Float64}, N)
+    t += dt
+    push!(time_series_1, u[47])
+    push!(time_series_2, u[126])
+	push!(time_series_3, u[225])
+	push!(time_series_4, u[324])
+	push!(time_series_5, u[423])
+	push!(time_series_6, u[512])
+
+	# Saving the full wavefunction for certain time steps
+	if t in [3.0, 3.5 ,4.0, 4.5, 5.0, 5.5, 6.0, 6.5, 7.0, 7.5, 8.0, 8.5, 9.0, 9.5, 10.0]
+		dataset = string("psi_", Int(t))
+		file[dataset] = u
+    end
 
 
-		#########################################################################
-		# Eigenstate Entanglement Entropy
-		#########################################################################
-
-		for j in 1:Ntot
-	
-			#computing the half chain entanglement entropy
-	
-			entanglement_ee[i]=entanglement_ee[i]+EntanglementEntropy(eigvecA[:,j], L)
-	
-		end
-
-
-		#########################################################################
-		# Lazarides-Luitz Staistics 
-		#########################################################################
-
-		Corr=LazadiresDiagram(eigA,eigvecA);
-
-		histogram_diag .+=histgram(diag(Corr), -1:0.001:1)
-		histogram_pi_diag .+=histgram(pi_diag(Corr), -1:0.001:1)
-
-		for j in 1:16
-				histogram_offdiag[j,:] .+=histgram(offdiag(Corr,j), -1:0.001:1)
-				histogram_pi_offdiag[j,:] .+=histgram(pi_offdiag(Corr,j), -1:0.001:1)
-		end
-
-
-		## Itr loop ends here
-	end
-
-	# normalising the Lazarides-Luitz statistics
-
-	histogram_diag=histogram_diag/Itrnumb
-	histogram_pi_diag=histogram_pi_diag/Itrnumb
-
-	histogram_offdiag=histogram_offdiag/Itrnumb
-	histogram_pi_offdiag=histogram_pi_offdiag/Itrnumb
-
-	#saving the Lazarides-Luitz statistics
-
-	file["L\$(L)/theta\$(theta)/epsilon"*first("\$(epsilon)",5)*"/Histogram/Diag"]=histogram_diag
-	file["L\$(L)/theta\$(theta)/epsilon"*first("\$(epsilon)",5)*"/Histogram/PiDiag"]=histogram_pi_diag
-
-	file["L\$(L)/theta\$(theta)/epsilon"*first("\$(epsilon)",5)*"/Histogram/OffDiag"]=histogram_offdiag
-	file["L\$(L)/theta\$(theta)/epsilon"*first("\$(epsilon)",5)*"/Histogram/PiOffDiag"]=histogram_pi_offdiag
-
-
-	## epsilon loop ends here
-end
-
-levelspacing=levelspacing/Itrnumb
-entanglement_ee=entanglement_ee/(Itrnumb*Ntot)
-
-file["L\$(L)/theta\$(theta)/Levelspacing"]=levelspacing;
-file["L\$(L)/theta\$(theta)/EntanglementEE"]=entanglement_ee;
-	
-close(file)	
-	
+	file["psi_77/$(itr)"] = time_series_1
+	file["psi_126/$(itr)"] = time_series_2
+	file["psi_225/$(itr)"] = time_series_3
+	file["psi_324/$(itr)"] = time_series_4
+	file["psi_423/$(itr)"] = time_series_5
+	file["psi_512/$(itr)"] = time_series_6
 
 
 
+close(file)
 
-
-
-EOF
 
 done 
 
