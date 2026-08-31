e20cf9d7b98b72a8fb98838f9db958177169aa0c
sagnik <sghosh@uni-bonn.de>
2026-08-31 15:06:00 +0200

    [mbl_dtc] cat_env, cat_run

---
+++ mbl_dtc/cat_env.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+#creates job iteratively 
+
+cd ~/local/
+
+L=12
+Itrnumb=1
+
+for (( i1=1; i1<=Itrnumb; i1++ ))
+do
+
+
+
+
+cat <<EOF >job_file_mbldtcL${L}_${i1}.jdl
+
+#Job Script to be submitted using HTCondor
+        
+Executable              = run_file_mbldtcL${L}_${i1}.sh
+JobBatchName            = mbldtc: L=${L}, Itr=${i1}                
+Environment             = ClusterId=\$(ClusterId);Process=\$(Process);SubHost=$ENV(SUBHOST);
+        
+Arguments		= 8
+        
+Universe                = vanilla
+
+Transfer_executable     = 
+Transfer_input_files    = 
+Transfer_output_files   =
+
+
+Error                   = log/err.\$(ClusterId).\$(Process)
+Output                  = log/out.\$(ClusterId).\$(Process)
+Log                     = 
+
+Request_memory          = 12  GB
+Request_cpus            = 1
+        
+Request_disk            = 2 GB
+
++CephFS_IO  = "low"
+        
++MaxRuntimeHours	= 48
+        
++ContainerOS            = "Debian11"
+
+# Submit job
+Queue 1        
+            
+EOF
+
+
+
+
+done
+
+
+
---
+++ mbl_dtc/cat_run.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+#creates job iteratively 
+
+cd ~/local/
+
+L=12
+Itrnumb=1
+
+for (( i1=1; i1<=Itrnumb; i1++ ))
+do
+
+
+
+
+
+cat <<EOF >run_file_mbldtcL${L}_${i1}.sh
+#!/bin/bash
+     
+  
+     
+# Goes to the job work direcrtory     
+            
+cd /jwd			  
+source /etc/profile
+
+
+
+#Copies, Extracts and removes the Julia tarball 
+            
+cp \$BUDDY/BAF/mbl_dtc/.julia/julia-1.9.4-08-08-24.tar.gz ./
+
+
+
+tar -xf julia-1.9.4-08-08-24.tar.gz
+rm -f julia-1.9.4-08-08-24.tar.gz
+
+lscpu --json | grep "Model name" | awk -F '"' '{print $8}' > model_name.txt
+
+
+#Loads Julia
+module load julia/1.9.4
+
+
+
+
+#Job submission 
+
+cp \$BUDDY/mbldtc_L${L}_${i1}.jl ./
+cp -rf \$BUDDY/BAF/header ./
+
+# set number of threads
+export OPENBLAS_NUM_THREADS=1
+export OMP_NUM_THREADS=1
+export JULIA_NUM_THREADS=1
+
+# Do the real thing here
+julia mbldtc_L${L}_${i1}.jl
+rm mbldtc_L${L}_${i1}.jl
+
+
+
+# Clean up the working directory
+
+cp  *hdf5 /cephfs/user/sghosh/data/.
+rm -rf *
+EOF
+
+
+
+
+
+done
+
+
+
