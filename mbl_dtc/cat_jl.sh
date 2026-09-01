5f054032e8b62be3b35a034edd84403ebb9b5bb0
sagnik <sghosh@uni-bonn.de>
2026-09-01 10:27:42 +0200

    [BAF_RERUN] L10, theta=0.3

--- mbl_dtc/cat_jl.sh
+++ mbl_dtc/cat_jl.sh
@@ -3,8 +3,8 @@
 cd $BUDDY
 
 L=10
-thetarun=0.0
-Itrnumb=400
+thetarun=0.3
+Itrnumb=200
 
 #cd $BUDDY
 
--- mbl_dtc/cat_run.sh
+++ mbl_dtc/cat_run.sh
@@ -5,8 +5,8 @@
 cd ~/local/
 
 L=10
-thetarun=0.0
-Itrnumb=400
+thetarun=0.3
+Itrnumb=200
 
 for (( i1=1; i1<=Itrnumb; i1++ ))
 do
