d7f4899aa1c2eddec56a1f1642786f98b06a1e55
sagnik <sghosh@uni-bonn.de>
2026-09-01 10:24:33 +0200

    [BAF_RERUN] L10, theta=0.0

--- mbl_dtc/cat_env.sh
+++ mbl_dtc/cat_env.sh
@@ -4,9 +4,9 @@
 
 cd ~/local/
 
-L=12
-thetarun=0.3
-Itrnumb=500
+L=10
+thetarun=0.0
+Itrnumb=400
 
 for (( i1=1; i1<=Itrnumb; i1++ ))
 do
--- mbl_dtc/cat_jl.sh
+++ mbl_dtc/cat_jl.sh
@@ -2,9 +2,9 @@
 
 cd $BUDDY
 
-L=12
-thetarun=0.3
-Itrnumb=500
+L=10
+thetarun=0.0
+Itrnumb=400
 
 #cd $BUDDY
 
@@ -35,7 +35,7 @@ include(".header/Header.jl")
 
 L=${L};
 theta=${thetarun};
-Itrnumb=1;
+Itrnumb=10;
 Ntot=2^L;
 
 epsilonlist=[0.0,0.025,0.05,0.075,0.1,0.125,0.15,0.175,0.2,0.225,0.25,0.275,0.3,0.325,0.35,0.375,0.4,0.425,0.45,0.475,0.5,0.525,0.55,0.575,0.6,0.625,0.65,0.675,0.7,0.725,0.75,0.775,0.8,0.825,0.85,0.875,0.9,0.925,0.95,0.975,1.0]
@@ -46,7 +46,7 @@ global entanglement_ee=fill(0.0,length(epsilonlist))
 
 
 
-file=h5open("mbldtc_L8_theta_\$(theta)_${i1}.hdf5","cw")
+file=h5open("mbldtc_L${L}_theta_${theta}_${i1}.hdf5","cw")
 attrs=attributes(file)
 
 
